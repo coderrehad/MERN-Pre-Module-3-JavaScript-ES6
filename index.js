4bba19e4970f6aadda17a8f59c8a915684b50789
coderrehad <engr.rehad@gmail.com>
2024-02-17 15:38:45 +0600

    videos-6-10 done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Hello World")
+//Strict Mode
+//'use strict'
+
+function MyFunc() {
+    return name = "Rehad"
+}
+
+console.log(MyFunc())
+
+//Spread Operator
+let poorCountry = ['Bangladesh','Pakistan','Italy']
+let richCountry = [...poorCountry,'America','SriLanka','Singapore']
+
+//console.log(richCountry)
+
+
+//Without Spread Operator
+poorCountry.push(richCountry);
+
+//console.log(richCountry)
+
+//Rest Parameter
+function RestFunc(...numbers) {
+    return numbers
+}
+
+console.log(RestFunc(2,4,3,6,5,7,8,9))
+
+//Dynamic Function
+let f = function (a){
+    return a
+}
+
+console.log(f(10))
