8c14f28ced8557f84fa318491e0c219ab59bde00
coderrehad <engr.rehad@gmail.com>
2024-02-19 19:36:36 +0600

    init repo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function MyFunc() {
     return name = "Rehad"
 }
 
-console.log(MyFunc())
+//console.log(MyFunc())
 
 //Spread Operator
 let poorCountry = ['Bangladesh','Pakistan','Italy']
@@ -24,11 +24,96 @@ function RestFunc(...numbers) {
     return numbers
 }
 
-console.log(RestFunc(2,4,3,6,5,7,8,9))
+//console.log(RestFunc(2,4,3,6,5,7,8,9))
 
 //Dynamic Function
 let f = function (a){
     return a
 }
 
-console.log(f(10))
+//console.log(f(10))
+
+
+// JavaScript Variables
+var name = "Rehad"
+var name = "Shawn"
+
+let MyName = "Rehad"
+MyName = "Shawn"
+
+const City = "Pabna"
+
+//console.log(City)
+
+// Variable Global Scope
+let size = "M"
+
+function GlobalScope() {
+    //console.log(size)
+}
+GlobalScope()
+
+
+// Variable Local Scope
+function LocalScope() {
+    let size = "M"
+    //console.log(size)
+}
+LocalScope()
+
+// Variable Hosting
+color = "Red";
+//console.log(color)
+
+var color;
+
+// Loop Concept
+let i;
+for (i=0;i<=10;i++){
+    //console.log(i)
+}
+
+// For Loop
+let NameArr = ['Rehad','Shawn','Rabbil']
+for(let item of NameArr){
+    //console.log(item)
+}
+
+// JavaScript Object
+
+let Men = {
+    name:"Bill Gates",
+    Address:"Medina, Washington",
+    Age:"68",
+    Dress:{
+        Shirt:true,
+        Pant:true,
+        Shoe:true,
+        Cap:false
+    }
+}
+
+console.log(Men['Dress']['Shirt'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
